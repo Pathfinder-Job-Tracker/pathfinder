37811f839ffcd436cb5126f778af9c01050e6fb0
haydencarroll1 <carrollhayden8@gmail.com>
2024-09-28 22:54:48 +0100

    Initial Commit - Creating React Elements

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# Pathfinder
+Chrome-Extension
---
+++ src/App.js
@@ -0,0 +1,13 @@
+// src/App.js
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>PathFinder</h1>
+      <p>Welcome to the PathFinder Chrome Extension</p>
+    </div>
+  );
+}
+
+export default App;
